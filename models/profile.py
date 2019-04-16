3529fce46e9afa9a4a99977a744719a12b29bd0d
dongweiming <ciici1234@hotmail.com>
2019-04-16 22:45:10 +0800

    Close #24

--- models/profile.py
+++ models/profile.py
@@ -2,37 +2,39 @@ import os
 import json
 from pathlib import Path
 
-from jsonschema import validate
+import attr
 
 from config import HERE
 from .mc import cache, clear_mc
 from .utils import AttrDict
 
-schema = {
-    "type": "object",
-    "properties": {
-        "intro": {"type": "string"},
-        "github_url": {"type": "string"},
-        "avatar": {"type": "string"},
-        "linkedin_url": {"type": "string"}
-    },
-}
 MC_KEY_PROFILE = 'profile'
 PROFILE_FILE = 'profile.json'
 
 
-@cache(MC_KEY_PROFILE)
-async def get_profile():
-    file = Path(HERE) / PROFILE_FILE
-    if not os.path.exists(file):
-        return {}
+@attr.s
+class Profile:
+    intro = attr.ib(default='', validator=attr.validators.instance_of(str))
+    github_url = attr.ib(default='',
+                         validator=attr.validators.instance_of(str))
+    avatar = attr.ib(default='', repr=False,
+                     validator=attr.validators.instance_of(str))
+    linkedin_url = attr.ib(default='', repr=False,
+                           validator=attr.validators.instance_of(str))
 
-    with open(file) as f:
-        return AttrDict(json.load(f))
+    @classmethod
+    @cache(MC_KEY_PROFILE)
+    async def get(cls):
+        file = Path(HERE) / PROFILE_FILE
+        if not os.path.exists(file):
+            return {}
 
+        with open(file) as f:
+            return attr.asdict(cls(**json.load(f)))
 
-async def set_profile(**kw):
-    validate(kw, schema)
-    with open(Path(HERE) / PROFILE_FILE, 'w') as f:
-        json.dump(kw, f)
-        await clear_mc(MC_KEY_PROFILE)
+    @classmethod
+    async def set(cls, **kw):
+        profile = cls(**kw)
+        with open(Path(HERE) / PROFILE_FILE, 'w') as f:
+            json.dump(attr.asdict(profile), f)
+            await clear_mc(MC_KEY_PROFILE)
