a1a5dfa631a6cce3d78b3344730c35aab5a76845
dongweiming <ciici1234@hotmail.com>
2019-04-18 22:28:42 +0800

    support mention

---
+++ models/mention.py
@@ -0,0 +1,29 @@
+import re
+
+from tortoise.query_utils import Q
+
+from models.user import GithubUser
+
+MENTION_LIMIT = 20
+_METION_UID_RE = re.compile(r'@([a-zA-Z0-9][-.\w]*?)(?![-.\w])')
+EMAIL_SUBJECT = '有人在 {title} 的评论中提到了你!'
+EMAIL_BODY = '''Hi, {name}:
+
+有人在 {site} 的 {subject} 这篇文章下的评论中提到了你! 快去看看把~
+'''
+
+
+class Mention:
+    @staticmethod
+    def _parse_mention_names(author, content=''):
+        names = set(name for name in _METION_UID_RE.findall(content)
+                    if name and name != author.username)
+        return list(names)[:MENTION_LIMIT]
+
+    @classmethod
+    async def get_mention_users(cls, content, author_id):
+        author = await GithubUser.cache(author_id)
+        if not author:
+            return []
+        mention_names = cls._parse_mention_names(author, content)
+        return await GithubUser.filter(Q(username__in=mention_names))
