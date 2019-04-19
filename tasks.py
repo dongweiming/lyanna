5f13fd180ffc7df9f8c01e8a748614d03b4bc568
dongweiming <ciici1234@hotmail.com>
2019-04-19 11:05:20 +0800

    Close #20

--- tasks.py
+++ tasks.py
@@ -1,26 +1,34 @@
-import smtplib
+import asyncio
+from functools import wraps
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-try:
-    from contextlib import asynccontextmanager
-except ModuleNotFoundError:
-    from async_generator import asynccontextmanager
-
+import aiosmtplib
 from aiotasks import build_manager
+from sanic_mako import render_string
 
-from ext import init_db, mako
+from app import app  # noqa
+from ext import init_db
 from models.blog import Post
-from models.mention import Mention, EMAIL_SUBJECT, EMAIL_BODY
+from models.mention import Mention, EMAIL_SUBJECT
 from config import (MAIL_SERVER, MAIL_PORT, MAIL_USERNAME,
-                    MAIL_PASSWORD, REDIS_URL, SITE_TITLE)
+                    MAIL_PASSWORD, REDIS_URL, SITE_TITLE, BLOG_URL)
 
 manager = build_manager(REDIS_URL)
 CAN_SEND = all((MAIL_SERVER, MAIL_USERNAME, MAIL_PASSWORD))
 
 
+def with_context(f):
+    @wraps(f)
+    async def _deco(*args, **kwargs):
+        await init_db()
+        result = await f(*args, **kwargs)
+        return result
+    return _deco
+
+
 @manager.task()
-async def send_email(subject, content, send_to):
+async def send_email(subject, html, send_to):
     if not CAN_SEND:
         return
 
@@ -28,19 +36,20 @@ async def send_email(subject, content, send_to):
     msg['Subject'] = subject
     msg['From'] = MAIL_USERNAME
     msg['To'] = send_to
-    msg.attach(MIMEText(message, 'html'))
+    msg.attach(MIMEText(html, 'html'))
 
-    s = smtplib.SMTP_SSL(MAIL_SERVER, port=MAIL_PORT)
-    s.login(MAIL_USERNAME, MAIL_PASSWORD)
-    if not isinstance(send_to, list):
-        send_to = [send_to]
-    s.send_message(msg)
-    s.quit()
+    loop = asyncio.get_event_loop()
+    smtp = aiosmtplib.SMTP(hostname=MAIL_SERVER, port=MAIL_PORT,
+                           loop=loop, use_tls=True)
+    await smtp.connect()
+    await smtp.login(MAIL_USERNAME, MAIL_PASSWORD)
+    await smtp.send_message(msg)
+    await smtp.quit()
 
 
 @manager.task()
+@with_context
 async def mention_users(post_id, content, author_id):
-    await init_db()
     post = await Post.cache(post_id)
     if not post:
         return
@@ -50,6 +59,13 @@ async def mention_users(post_id, content, author_id):
         if not email:
             continue
         subject = EMAIL_SUBJECT.format(title=post.title)
-        content = EMAIL_BODY.format(site=SITE_TITLE, subject=post.title,
-                                    name=user.username)
-        await send_email.delay(subject, content, email)
+        request = type('Request', (), {'app': app, 'get': lambda self: False})
+        html = await render_string(
+            'email/mention.html', request,
+            {
+                'username': user.username,
+                'site_url': BLOG_URL,
+                'site_name': SITE_TITLE,
+                'post': post
+            })
+        await send_email.delay(subject, html, email)
