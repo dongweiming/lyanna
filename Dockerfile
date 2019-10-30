d427154eae43e31524440157a779bb9de91f89ad
dongweiming <ciici1234@hotmail.com>
2019-10-30 17:04:33 +0800

    Add kubernetes support

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,8 @@ RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositorie
     && apk add git gcc musl-dev libffi-dev openssl-dev make
 WORKDIR /install
 COPY requirements.txt /requirements.txt
-RUN pip install -i https://mirrors.ustc.edu.cn/pypi/web/simple -r /requirements.txt \
+RUN pip install -i https://pypi.tuna.tsinghua.edu.cn/simple \
+    --trusted-host pypi.tuna.tsinghua.edu.cn -r /requirements.txt \
     && mkdir -p /install/lib/python3.7/site-packages \
     && cp -rp /usr/local/lib/python3.7/site-packages /install/lib/python3.7
 
@@ -13,3 +14,5 @@ COPY --from=build /install/lib /usr/local/lib
 COPY --from=build /install/src /usr/local/src
 WORKDIR /app
 COPY . /app
+COPY --from=build /usr/local/bin/gunicorn /app/gunicorn
+COPY --from=build /usr/local/bin/arq /app/arq
--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import os
+import redis
 
-DB_URL = 'mysql://root:@localhost:3306/test?charset=utf8'
-REDIS_URL = 'redis://localhost:6379'
-DEBUG = False
+DB_URL = os.getenv('DB_URL', 'mysql://root:@localhost:3306/test?charset=utf8')
+REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379')
+DEBUG = os.getenv('DEBUG', '').lower() in ('true', 'y', 'yes', '1')
 WTF_CSRF_SECRET_KEY = 123
 AUTH_LOGIN_ENDPOINT = 'index.login'
-MEMCACHED_HOST = '127.0.0.1'
+MEMCACHED_HOST = os.getenv('MEMCACHED_HOST', '127.0.0.1')
 MEMCACHED_PORT = 11211
 
 oauth_redirect_path = '/oauth'
@@ -42,7 +43,21 @@ MAIL_PASSWORD = ''
 
 BLOG_URL = 'https://example.com'
 
+# Redis sentinel
+REDIS_SENTINEL_SERVICE_HOST = None
+REDIS_SENTINEL_SERVICE_PORT = 26379
+
 try:
     from local_settings import *  # noqa
 except ImportError:
     pass
+
+redis_sentinel_host = os.getenv('REDIS_SENTINEL_SVC_HOST') or REDIS_SENTINEL_SERVICE_HOST  # noqa
+if redis_sentinel_host:
+    redis_sentinel_port = os.getenv('REDIS_SENTINEL_SVC_PORT',
+                                    REDIS_SENTINEL_SERVICE_PORT)
+    from redis.sentinel import Sentinel
+    sentinel = Sentinel([(redis_sentinel_host, redis_sentinel_port)],
+                        socket_timeout=0.1)
+    redis_host, redis_port = sentinel.discover_master('mymaster')
+    REDIS_URL = f'redis://{redis_host}:{redis_port}'
