451f80512ca9622a7a6166de9a6356e779ada721
dongweiming <ciici1234@hotmail.com>
2019-11-20 08:18:59 +0800

    Add topic admin page

---
+++ admin/src/views/topic/edit.vue
@@ -0,0 +1,12 @@
+<template>
+  <topic-detail :is-edit="true"/>
+</template>
+
+<script>
+import TopicDetail from './components/TopicDetail'
+
+export default {
+  name: 'EditForm',
+  components: { TopicDetail }
+}
+</script>
